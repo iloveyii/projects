40f646f2a9d2b07e511c10027b7457ad9866802e
Hazrat Ali <iloveyii@yahoo.com>
2019-04-28 15:33:50 +0200

    Added firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,18 @@
+import  firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC0Q2xFnmVhwCogJFoxDR2bvVJh1ZZC7gI",
+    authDomain: "eshop-aa94e.firebaseapp.com",
+    databaseURL: "https://eshop-aa94e.firebaseio.com",
+    projectId: "eshop-aa94e",
+    storageBucket: "eshop-aa94e.appspot.com",
+    messagingSenderId: "1085585020624"
+};
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots : true });
+
+export default firebase;
