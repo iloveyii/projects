287f922bba93e9c0d907a7c8ba874b88e7f8349a
Hazrat Ali <iloveyii@yahoo.com>
2019-04-28 13:16:08 +0200

    Added sign up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 
 import './App.css';
 import Dashboard from "./components/dashboard/Dashboard";
 import ProjectDetails from './components/projects/ProjectDetails';
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
-                <Navbar />
+                <Navbar/>
                 <Switch>
-                    <Route exact path='/' component={Dashboard}></Route>
-                    <Route path='/project/:id' component={ProjectDetails}></Route>
+                    <Route exact path='/' component={Dashboard}/>
+                    <Route path='/project/:id' component={ProjectDetails}/>
+                    <Route path='/signin' component={SignIn}/>
+                    <Route path='/signup' component={SignUp}/>
                 </Switch>
             </div>
         </BrowserRouter>
