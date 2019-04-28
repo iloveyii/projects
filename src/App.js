b6389834eea92465794c62692414d21df997dced
Hazrat Ali <iloveyii@yahoo.com>
2019-04-28 12:02:16 +0200

    Added details

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from "./components/dashboard/Dashboard";
+import ProjectDetails from './components/projects/ProjectDetails';
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
                 <Navbar />
+                <Switch>
+                    <Route exact path='/' component={Dashboard}></Route>
+                    <Route path='/project/:id' component={ProjectDetails}></Route>
+                </Switch>
             </div>
         </BrowserRouter>
 
