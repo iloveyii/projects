54bd1c60faead52d18aa48ff55e1d2578c962445
Hazrat Ali <iloveyii@yahoo.com>
2019-04-30 22:12:17 +0200

    Deployed

---
+++ dist/precache-manifest.af501d04d53c88c2e5d4d256529981e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a0d60944dd6916d9c916e6e6b47faf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0074e92b619017bb271f",
+    "url": "/static/css/main.5d72b650.chunk.css"
+  },
+  {
+    "revision": "09988328fbf0d7a6860f",
+    "url": "/static/js/2.85aea931.chunk.js"
+  },
+  {
+    "revision": "0074e92b619017bb271f",
+    "url": "/static/js/main.f070eef9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -31,7 +31,7 @@ class Dashboard extends React.Component {
     }
 
     render() {
-        const {projects, auth} = this.props;
+        const {projects, auth, notifications} = this.props;
 
         if(!auth.uid) return <Redirect to={'/signin'} />
         
@@ -42,7 +42,7 @@ class Dashboard extends React.Component {
                         <ProjectList projects={projects}/>
                     </div>
                     <div className="col s12 m5 offset-m1">
-                        <Notification />
+                        <Notification notifications={notifications} />
                     </div>
                 </div>
             </div>
@@ -55,6 +55,7 @@ const mapStateToProps = (state) => {
     // console.log(state);
     return {
         projects: state.firestore.ordered.projects,
+        notifications: state.firestore.ordered.notifications,
         auth: state.firebase.auth
     }
 };
@@ -62,6 +63,7 @@ const mapStateToProps = (state) => {
 export default compose(
     connect(mapStateToProps),
     firestoreConnect([
-        { collection: 'projects' }
+        { collection: 'projects', orderBy: ['createdAt', 'desc'] },
+        { collection: 'notifications', limit: 50, orderBy : ['time', 'desc'] }
     ])
 )(Dashboard)
