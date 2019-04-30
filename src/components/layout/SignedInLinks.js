f40d6e20bd853f2551262586beddf6e5a935339e
Hazrat Ali <iloveyii@yahoo.com>
2019-04-30 08:10:17 +0200

    Show initials

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -9,7 +9,7 @@ const SignedInLinks = (props) => {
         <ul className="right">
             <li><NavLink to='/create'>New Project</NavLink></li>
             <li><a onClick={props.signOut} to='/'>Log Out</a></li>
-            <li><NavLink to='/' className='btn btn-floating pink lighten-1'>AA</NavLink></li>
+            <li><NavLink to='/' className='btn btn-floating pink lighten-1'>{props.profile.initials}</NavLink></li>
         </ul>
     )
 };
